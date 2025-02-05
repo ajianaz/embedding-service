42545e52c8b62d473e944792e201f72f363b6a21
Aji Anaz <aji.anaz@gmail.com>
2025-02-06 00:12:17 +0700

    Update : Remove workflow main, add new for bge-small-en & all-MiniLM-L6-v2

--- app/embedder.py
+++ app/embedder.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from sentence_transformers import SentenceTransformer
 
 app = Flask(__name__)
-model = SentenceTransformer("all-MiniLM-L6-v2")  # Model ringan
+model = SentenceTransformer("BAAI/bge-small-en")
 
 @app.route("/embed", methods=["POST"])
 def embed():
