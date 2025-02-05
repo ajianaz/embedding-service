224cba97ae5e951c6a58e416f9b35016eda14e8b
Aji Anaz <aji.anaz@gmail.com>
2025-02-06 00:39:50 +0700

    Update : Build for model all-MiniLM-L6-v2

--- app/embedder.py
+++ app/embedder.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from sentence_transformers import SentenceTransformer
 
 app = Flask(__name__)
-model = SentenceTransformer("BAAI/bge-small-en")
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 @app.route("/embed", methods=["POST"])
 def embed():
